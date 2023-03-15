ad9c12f379b875431127e01370eb3e50dde5c388
Anton Demkin <avdemkin@avito.ru>
2023-03-15 17:06:02 +0400

    Simplify View for demonstration reasons

--- lib/views.py
+++ lib/views.py
@@ -9,29 +9,33 @@ from lib.image import open_image
 
 class IndexView(View):
     async def get(self) -> Response:
-        return render_template("index.html", self.request, {})
+        # return render_template("index.html", self.request, {})
+        return Response(
+            text='<h1>Hello</h1>',
+            # headers={'content-type': 'text/html'},
+        )
 
-    async def post(self) -> Response:
-        try:
-            form = await self.request.post()
-            image = open_image(form["image"].file)
-            draw = PolygonDrawer(image)
-            model = self.request.app["model"]
-            words = []
-            for coords, word, accuracy in model.readtext(image):
-                draw.highlight_word(coords, word)
-                cropped_img = draw.crop(coords)
-                cropped_img_b64 = image_to_img_src(cropped_img)
-                words.append(
-                    {
-                        "image": cropped_img_b64,
-                        "word": word,
-                        "accuracy": accuracy,
-                    }
-                )
-            image_b64 = image_to_img_src(draw.get_highlighted_image())
-            ctx = {"image": image_b64, "words": words}
-            return render_template("index.html", self.request, ctx)
-        except Exception as err:
-            ctx = {"error": str(err)}
-            return render_template("index.html", self.request, ctx)
+#     async def post(self) -> Response:
+#         try:
+#             form = await self.request.post()
+#             image = open_image(form["image"].file)
+#             draw = PolygonDrawer(image)
+#             model = self.request.app["model"]
+#             words = []
+#             for coords, word, accuracy in model.readtext(image):
+#                 draw.highlight_word(coords, word)
+#                 cropped_img = draw.crop(coords)
+#                 cropped_img_b64 = image_to_img_src(cropped_img)
+#                 words.append(
+#                     {
+#                         "image": cropped_img_b64,
+#                         "word": word,
+#                         "accuracy": accuracy,
+#                     }
+#                 )
+#             image_b64 = image_to_img_src(draw.get_highlighted_image())
+#             ctx = {"image": image_b64, "words": words}
+#             return render_template("index.html", self.request, ctx)
+#         except Exception as err:
+#             ctx = {"error": str(err)}
+#             return render_template("index.html", self.request, ctx)
