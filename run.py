d0e58f226eff7c4044fe816a735f61204a6d8057
ekulbacki <>
2024-04-24 20:35:52 +0300

    initial

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from lib.app import create_app
 
 def main() -> None:
     app = create_app()
-    run_app(app, port=8000)
+    run_app(app, host='0.0.0.0', port=8000)
 
 
 if __name__ == "__main__":
