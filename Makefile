020a942310166f58427a0cfe2867ffa14ed2a063
Slesarenko <slesarenko221999@gmail.com>
2024-04-29 15:03:29 +0300

    hw done

--- Makefile
+++ Makefile
@@ -16,7 +16,8 @@ build:
 
 dev: build
 	@echo 'Run dev server with live reload- refer to dev server address.'
-	@docker run --rm -v $(PWD):/app \
+	@docker run --rm \
+		-v $(PWD):/app \
 		-p 0.0.0.0:8000:8000 \
 		-p 0.0.0.0:8001:8001 \
 		-it ${IMAGE} \
--- lib/views.py
+++ lib/views.py
@@ -1,47 +1,39 @@
 from aiohttp.web import Response
 from aiohttp.web import View
 
-# from aiohttp_jinja2 import render_template
+from aiohttp_jinja2 import render_template
 
-# from lib.image import image_to_img_src
-# from lib.image import PolygonDrawer
-# from lib.image import open_image
+from lib.image import image_to_img_src
+from lib.image import PolygonDrawer
+from lib.image import open_image
 
 
 class IndexView(View):
     template = "index.html"
 
     async def get(self) -> Response:
-        # return render_template(self.template, self.request, {})
-        return Response(
-            text="""
-                <h1>Всё работает</h1>
-                <p>теперь загляни в <pre>lib/views.py</pre></p>
-            """,
-            headers={"content-type": "text/html"},
-        )
+        return render_template(self.template, self.request, {})
 
-
-#     async def post(self) -> Response:
-#         try:
-#             form = await self.request.post()
-#             image = open_image(form["image"].file)
-#             draw = PolygonDrawer(image)
-#             model = self.request.app["model"]
-#             words = []
-#             for coords, word, accuracy in model.readtext(image):
-#                 draw.highlight_word(coords, word)
-#                 cropped_img = draw.crop(coords)
-#                 cropped_img_b64 = image_to_img_src(cropped_img)
-#                 words.append(
-#                     {
-#                         "image": cropped_img_b64,
-#                         "word": word,
-#                         "accuracy": accuracy,
-#                     }
-#                 )
-#             image_b64 = image_to_img_src(draw.get_highlighted_image())
-#             ctx = {"image": image_b64, "words": words}
-#         except Exception as err:
-#             ctx = {"error": str(err)}
-#         return render_template(self.template, self.request, ctx)
+    async def post(self) -> Response:
+        try:
+            form = await self.request.post()
+            image = open_image(form["image"].file)
+            draw = PolygonDrawer(image)
+            model = self.request.app["model"]
+            words = []
+            for coords, word, accuracy in model.readtext(image):
+                draw.highlight_word(coords, word)
+                cropped_img = draw.crop(coords)
+                cropped_img_b64 = image_to_img_src(cropped_img)
+                words.append(
+                    {
+                        "image": cropped_img_b64,
+                        "word": word,
+                        "accuracy": accuracy,
+                    }
+                )
+            image_b64 = image_to_img_src(draw.get_highlighted_image())
+            ctx = {"image": image_b64, "words": words}
+        except Exception as err:
+            ctx = {"error": str(err)}
+        return render_template(self.template, self.request, ctx)
