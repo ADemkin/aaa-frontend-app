67c4a4c10406976072767681e602ba734cb9918b
Ruslan Melnic <rmus@MacBook-Pro-Ruslan.local>
2024-05-22 23:34:04 +0300

    last infra hw

--- lib/app.py
+++ lib/app.py
@@ -6,21 +6,31 @@ from aiohttp.web import Application
 
 from lib import views
 from lib.models import create_model
+# асинхронный код
 
 
 lib = Path("lib")
 
-
 def create_app() -> Application:
+    # центральный элемент фреймворка
+
     app = Application()
     # setup routes
     app.router.add_static("/static/", lib / "static")
+    # подключаем пути в роутер
+    # добавляем статику - картинки, jss, css - то, что пользователю
+    # показывается
     app.router.add_view("/", views.IndexView, name="index")
+    # добавляем хендлер на приложение, чтобы 
+    # заходя в корень приложения - попадали в обработчик
+
     # setup templates
     aiohttp_jinja2.setup(
         app=app,
         loader=jinja2.FileSystemLoader(lib / "templates"),
     )
+    # магия, которая позволяет из папки templates 
+    # загружать шаблоны с разными именами
     app["model"] = create_model()
     return app
 
