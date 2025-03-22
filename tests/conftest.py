1580fdc681e909d2e8ab9688a95c7f5b4fcf62d1
Anton Demkin <antondemkin@yandex.ru>
2025-03-22 13:34:55 +0400

    Simplify tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,37 @@
+from pathlib import Path
+from unittest.mock import MagicMock
+
 import pytest
-
 from fastapi import FastAPI
-from lib.app import app as _app
-
 from fastapi.testclient import TestClient
 
+from lib.app import app as _app
+from lib.models import Reader
+from lib.models import get_model
 
-@pytest.fixture
+
+@pytest.fixture()
 def app() -> FastAPI:
     return _app
 
 
-@pytest.fixture
+@pytest.fixture()
 def client(app) -> TestClient:
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture()
+def model_mock(app):
+    mock = MagicMock(spec=Reader)
+    app.dependency_overrides[get_model] = lambda: mock
+    yield mock
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture()
+def image_file():
+    image_path = Path("images/avito.jpg")
+    assert image_path.exists()
+    with open(image_path, "rb") as file:
+        yield file
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,55 +1,66 @@
-from unittest.mock import MagicMock
-
-# from aiohttp import FormData
 from http import HTTPStatus
-from lib.models import get_model
-from lib.models import Reader
-
-import pytest
 
 
-def test_index_page_contain_valid_multipart_form(client):
-    response = client.get("/")
+def test_integration(client, image_file):
+    response = client.post("/", files={"file": image_file})
     assert response.status_code == HTTPStatus.OK, response.text
     text = response.text.lower()
-    assert "<form " in text, "На странице нет формы"
-    assert 'method="post"' in text, "У формы не указан POST метод"
-    assert 'enctype="multipart/form-data"' in text, "у формы не указан enctype"
-    assert "submit" in text, "у формы нет кнопки отправки"
+    assert "avito" in text, f"слово 'avito' не найдено в {text}"
 
 
-@pytest.mark.parametrize(
-    "image_path,text_expected",
-    [
-        ("images/avito.jpg", "avito"),
-        # ('images/signs.jpg', 'albuquerque'),
-        # ('images/signs-small.jpg', 'angeles'),
-    ],
-)
-def test_if_sent_image_with_word_then_word_appear_in_response(
-    client,
-    image_path,
-    text_expected,
-):
-    with open(image_path, "rb") as file:
-        response = client.post("/", files={"file": file})
-    assert response.status_code == HTTPStatus.OK, response.text
-    text = response.text.lower()
-    assert text_expected in text, f"expected {text_expected} in {text}"
+class TestPageContent:
+    """
+    Этот тест проверяет разное поведение страницы в зависимости от ответа модели.
 
+    Такие тесты позволяют проверять поведение всей системы быстро, без
+    включения её медленных компонентов- в нашем случае модели.
+    """
 
-def test_if_sent_faulty_image_then_error_appear_in_response(app, client):
-    image_path = "images/error.jpg"
-    error_message = "something nasty happened"
-    # не смотря на то, что отправляется настоящее изображение,
-    # модель мокается, чтобы контролировать исключение - оно
-    # может быть разным на разных платформах
-    mock = MagicMock(spec=Reader)
-    mock.readtext.side_effect = Exception(error_message)
-    app.dependency_overrides[get_model] = lambda: mock
-    with open(image_path, "rb") as file:
-        response = client.post("/", files={"file": file})
-    app.dependency_overrides.clear()
-    assert response.status_code == HTTPStatus.OK, response.text
-    text = response.text.lower()
-    assert error_message in text, f"expected {error_message} in {text}"
+    @staticmethod
+    def test_index_page_contain_valid_multipart_form(client):
+        response = client.get("/")
+        assert response.status_code == HTTPStatus.OK, response.text
+        text = response.text.lower()
+        assert "<form " in text, "На странице нет формы"
+        assert 'method="post"' in text, "У формы не указан POST метод"
+        assert 'enctype="multipart/form-data"' in text, "у формы не указан enctype"
+        assert "submit" in text, "у формы нет кнопки отправки"
+
+    @staticmethod
+    def test_if_model_gives_error_then_response_containt_error_text(
+        client,
+        model_mock,
+        image_file,
+    ):
+        error_message = "какая-то ошибка"
+        model_mock.readtext.side_effect = Exception(error_message)
+        response = client.post("/", files={"file": image_file})
+        assert response.status_code == HTTPStatus.OK, response.text
+        text = response.text.lower()
+        assert error_message in text, "в ответе нет ошибки"
+
+    @staticmethod
+    def test_if_model_gives_words_then_response_contain_table(
+        client,
+        model_mock,
+        image_file,
+    ):
+        model_mock.readtext.return_value = [
+            ((0, 0, 10, 10), "avito", 0.99),
+        ]
+        response = client.post("/", files={"file": image_file})
+        assert response.status_code == HTTPStatus.OK, response.text
+        text = response.text.lower()
+        assert "<table" in text, "на странице нет таблицы"
+
+    @staticmethod
+    def test_if_model_gives_not_words_then_response_not_contain_table(
+        client,
+        model_mock,
+        image_file,
+    ):
+        model_mock.readtext.return_value = []
+        response = client.post("/", files={"file": image_file})
+        assert response.status_code == HTTPStatus.OK, response.text
+        text = response.text.lower()
+        assert "<table" not in text, "на странице не должно быть таблицы"
